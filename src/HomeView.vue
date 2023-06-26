088b6501798893acdc5c66e0320a37a2af1a6fb1
xiranran <2449500848@qq.com>
2023-06-27 07:20:33 +0800

    docs: :bug: 项目发生改变
    :bug: 项目发生改变
    
    ISSUES CLOSED: :bug: 项目发生改变

---
+++ src/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <header class="w-[97vw] h-[10vw] relative mt-6">
+            <div class="flex justify-around items-center">
+                <Icon icon="pepicons-pop:menu" color="white" class="text-[8vw]" />
+                <div class="relative">
+                    <input type="text" placeholder="足够 周星星" class="w-[55vw] h-10 rounded-3xl pl-8  border-amber-400">
+                    <Icon icon="carbon:search" color="Seashell3" class="text-[8vw] absolute bottom-[3px] left-[4px]" />
+                </div>
+                <Icon icon="ph:microphone-bold" color="white" class="text-[8vw]" />
+            </div>
+        </header>
+    
+        <section>
+            <!-- <div class="swiper mySwiper swiper-container">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">Slide 1</div>
+                  <div class="swiper-slide">Slide 2</div>
+                  <div class="swiper-slide">Slide 3</div>
+                  <div class="swiper-slide">Slide 4</div>
+                  <div class="swiper-slide">Slide 5</div>
+                  <div class="swiper-slide">Slide 6</div>
+                  <div class="swiper-slide">Slide 7</div>
+                  <div class="swiper-slide">Slide 8</div>
+                  <div class="swiper-slide">Slide 9</div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div> -->
+        </section>
+    </div>
+    
+    
+</template>
+<script>
+    
+</script>
+<style>
+    body{
+        background-color: #2d2c2c;
+    }
+    .swiper-container {
+        width: 100%;
+        height: 300px;
+      }
+      
+      .swiper-slide {
+        text-align: center;
+        font-size: 24px;
+        background-color: #f2f2f2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+</style>
