36aedc710e8d445335e1ec7f7a72095ec00562a5
xiranran <2449500848@qq.com>
2023-06-27 14:24:27 +0800

    docs(home): :rotating_light: 14:24提交
    :rotating_light: 14:24提交
    
    ISSUES CLOSED: #1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import Vue from 'vue';
 import './index.css';
+// import './swiper-bundle.min.js'
 import { Icon } from '@iconify/vue2';
 import HomeView from './HomeView.vue';
 Vue.component('Icon', Icon);
 const vm = new Vue({
-  el: '#app',
-  render:(h) => h(HomeView)
+    el: '#app',
+    data: {
+      swiper: null
+    },
+    render: h => h(HomeView),
+    mounted() {
+      this.initSwiper();
+    },
+    methods: {
+      initSwiper() {
+        this.swiper = new Swiper(".mySwiper", {
+          autoplay: true, // 自动播放
+          disableOnInteraction: false, // 鼠标交互后不停止自动播放
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">'  + "</span>";
+            },
+          },
+        });
+      }
+    }
 });
