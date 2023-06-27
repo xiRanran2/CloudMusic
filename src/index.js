4fa5332291b09f1006194145dfa864050ebd8d46
五行缺金 <12780990+xrr_324@user.noreply.gitee.com>
2023-06-27 09:24:56 +0000

    !4 6.27课堂范例 * Merge branch 'master' of gitee.com:xrr_324/6.25code into feat/0627 * docs: :alien: 6.27课堂范例 * feat: :lipstick: 14:27

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import Vue from 'vue';
-import './index.css';
-// import './swiper-bundle.min.js'
 import { Icon } from '@iconify/vue2';
-import HomeView from './HomeView.vue';
+import App from '@/App.vue';
+import MineView from '@/views/MineView.vue'
+Vue.component('MineView',MineView)
 Vue.component('Icon', Icon);
-const vm = new Vue({
-    el: '#app',
-    data: {
-      swiper: null
-    },
-    render: h => h(HomeView),
-    mounted() {
-      this.initSwiper();
-    },
-    methods: {
-      initSwiper() {
-        this.swiper = new Swiper(".mySwiper", {
-          autoplay: true, // 自动播放
-          disableOnInteraction: false, // 鼠标交互后不停止自动播放
-          pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            renderBullet: function (index, className) {
-              return '<span class="' + className + '">'  + "</span>";
-            },
-          },
-        });
-      }
-    }
+//vm就是根实例 root 具备el
+//但是其他实例不觉比
+//组件的嵌套  A组件出现在B组件的template中 A是B的子组件
+//如果一个组件没有父组件 ，则说明它是root组件
+/*
+组件树
+  root
+    App
+      IndexView
+      HomeView
+*/
+//vm.$parent  返回父组件
+//vm.$children  返回子组件
+//vm.$root  返回根组件
+new Vue({
+  el: '#app',
+  components:{App},
+  template:`<App/>`,
+  //实例已经创建 但是实例上的模板还没有渲染
+  created(){
+    console.log(this.$parent);  //[App]
+    console.log(this.$children);  //[App]
+  },
+  mounted(){
+    console.log(this.$children[0].$children);
+  }
 });
+
+// 组件分类
+// 页面组件 业务组件
+// 内置组件 第三方组件 自定义组件
+// 局部组件 全局组件 (引入文件、注册主键、使用标签) 全局组件
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 class="active">HomeView</h1>
+</template>
+
+<script>
+    //el配置只存在根实例中
+    export default {}
+</script>
+
+<style scoped>
+    .active{
+        background-color: pink;
+        color: aqua;
+        text-align: center;
+    }
+</style>
