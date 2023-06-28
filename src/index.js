e7ca7f9210c7f50b6b595b069cd12a7a872a94eb
xiranran <2449500848@qq.com>
2023-06-28 10:57:31 +0800

    feat: 10:57提交
    10:57提交
    
    BREAKING CHANGE:
    10:57提交
    
    ISSUES CLOSED: #1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue';
-import './index.css';
 import { Icon } from '@iconify/vue2';
-import indexView from './IndexView.vue'
+import App from '@/App.vue';
+import MineView from '@/views/MineView.vue'
+Vue.component('MineView',MineView)
 Vue.component('Icon', Icon);
-// 页面打开请求自动发送
-// 请求回来的数据还要渲染到页面上 （数据驱动视图）
-// 数据驱动视图变化的条件：数据必须是响应式数据（data）+ 数据必须通过模板语法绑定到模板中
-// vue中this的指向问题：methods中的所有函数（不要箭头函数）的this指向vm（vue的实例）
-const vm = new Vue({
+new Vue({
   el: '#app',
-  render:(h) => h(indexView)
+  components:{App},
+  template:`<App/>`,
+  //实例已经创建 但是实例上的模板还没有渲染
+  created(){
+    console.log(this.$parent);  //[App]
+    console.log(this.$children);  //[App]
+  },
+  mounted(){
+    console.log(this.$children[0].$children);
+  }
 });
-// 以_开头的变量名属性名是私有变量或私用属性，不希望其他人去使用
-// 一般情况下：我们在实例化vue传递的配置项 a => $a
-// el => $el
-// data => $data
-// afterInit ==> created
---
+++ src/views/HomeView.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <header class="w-[97vw] h-[10vw] relative  m-auto">
+            <div class="flex justify-around items-center">
+              <Icon icon="pepicons-pop:menu"  class="text-[6vw]" />
+              <div class="relative">
+                <input
+                  type="text"
+                  :placeholder="defaultSearch.showKeyword"
+                  v-model="userSearchKeywords"
+                  class="w-[70vw] h-[8vw] rounded-3xl pl-6 bg-[#c97d7d]"
+                />
+                <Icon
+                  icon="carbon:search"
+                  color="Seashell3"
+                  class="text-[6vw] absolute bottom-[2px] left-[4px]"
+                  @click.native="searchHandler(userSearchKeywords)"
+                />
+              </div>
+              <Icon icon="ph:microphone-bold"  class="text-[6vw]" />
+            </div>
+        </header>
+        <ul v-if="searchSuggestList.length">
+            <li v-for="item in searchSuggestList" :key="item.id" @click="searchHandler(item.name)">{{item.name}}</li>
+        </ul>
+    </div>
+    
+</template>
+
+<script>
+    import _ from 'lodash';
+    import {fetchSearchDefault,fetchSearchResult,fetchSearchSuggest} from '@/request'
+    export default {
+        data(){
+            return {
+                userSearchKeywords:'',
+                defaultSearch:{},
+                searchSuggestList:[],
+            }
+        },
+        methods:{
+            async searchHandler(keywords){
+                const res = await fetchSearchResult({
+                    keywords:keywords || this.defaultSearch.realkeyword
+                })
+                console.log(res)
+            },
+        },
+        async created (){
+            const res = await fetchSearchDefault();
+            this.defaultSearch = res.data.data;
+        },
+        watch:{
+            // async userSearchKeywords(keywords){
+            //     const res = await fetchSearchSuggest(keywords);
+            //     this.searchSuggestList = res.data.result.songs;
+            // },
+            userSearchKeywords:_.debounce(async function(keywords){
+                const res = await fetchSearchSuggest(keywords);
+                this.searchSuggestList = res.data.result.songs;
+            },3000)
+        },
+    }
+</script>
+
+<style scoped>
+    .active{
+        background-color: pink;
+        color: aqua;
+        text-align: center;
+    }
+</style>
