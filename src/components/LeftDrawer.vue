57c5f75065a2962dd1306d4757e67ff63d8064ab
xiranran <2449500848@qq.com>
2023-06-30 17:17:47 +0800

    feat: :fire: 提交的侧边栏和小轮播
    :fire: 提交的侧边栏和小轮播
    
    BREAKING CHANGE:
    :fire: 提交的侧边栏和小轮播
    
    ISSUES CLOSED: #1

---
+++ src/components/LeftDrawer.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- 插槽 -->
+    <Drawer :visible.sync="drawerList" direction="ltr">
+        
+    </Drawer>
+</template>
+
+<script>
+    // export default{
+    //     name:'LeftDrawer',
+    //     props:['leftDrawer'],
+    //     data(){
+    //         return {
+    //             drawerList:true,
+
+    //         }
+    //     }
+    // }
+</script>
